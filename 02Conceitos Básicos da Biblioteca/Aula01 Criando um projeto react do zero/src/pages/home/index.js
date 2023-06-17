52d9be85eb06c2219db5657d56e966ada4afc5c8
Jefferson Martendal de Barros <55723528+JeffersonMartendal@users.noreply.github.com>
2023-06-17 16:45:39 -0300

    Acrescentando aula

--- 02Conceitos Básicos da Biblioteca/Aula01 Criando um projeto react do zero/src/pages/home/index.js
+++ 02Conceitos Básicos da Biblioteca/Aula01 Criando um projeto react do zero/src/pages/home/index.js
@@ -1,7 +1,29 @@
-import {Header} from "../../components/Header"
+import { Header } from "../../components/Header"
+import background from '../../assets/background.png'
+
+import './styles.css'
+
 function App() {
     return <div className="App">
         <Header />
+        <div className="conteudo">
+            <img src={background} className="background" alt="background app"/>
+            <div className="info">
+            <div>
+                <input name="usuario" placeholder="@username" />
+                <button>Buscar</button>
+            </div>
+            <div className="perfil">
+                <img src="https://avatars.githubusercontent.com/u/55723528?v=4" className="profile" alt="imagen de perfil"/>
+                <div>
+                    <h3>Jefferson Martendal</h3>
+                    <span>@jeffersonmartendal</span>
+                    <p>Descrição</p>
+                </div>
+            </div>
+            <hr />
+            </div>
+        </div>
     </div>
 }
 
